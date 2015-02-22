9ea045afd31131ce8b9392dc47487f2b77ccc214
Hiroyuki OHARA <Hiroyuki.no22@gmail.com>
2015-02-22 16:08:39 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,281 @@
+qit.js
+======================================================================
+
+qit.js is a small iterator library for ES5/ES6 providing lazy operations
+on iterator/generator compatible with ES6 iterator protocol.
+
+Getting started
+----------------------------------------------------------------------
+
+qit.js has no external dependencies and is only 8kb minified.
+
+### In Browsers supporting ES5 / ES6 :
+
+```html
+<script src="path/to/qit.min.js"></script>
+```
+
+Example
+----------------------------------------------------------------------
+
+### Lazy map
+```javascript
+function square(x) {
+  console.log('square(' + x + ')');
+  return x * x;
+}
+
+var it = qit([1,2,3]).map(square);
+
+it.each(function(x){
+  console.log(x);
+});
+```
+
+output:
+```
+square(1)
+1
+square(2)
+4
+square(3)
+9
+```
+
+### Infinite sequence
+```javascript
+var nats = qit(1, Infinity);
+
+console.dir(nats.take(5).toArray());
+
+
+var fibs = qit([1, 1], function(e){
+  return [ e[1], e[0] + e[1] ]
+}).map(function(e){ return e[0] });
+
+console.dir(fibs.take(10).toArray());
+```
+
+output:
+
+```
+[ 1, 2, 3, 4, 5 ]
+[ 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 ]
+```
+
+### Working with ES6 generator
+```javascript
+function* fibonacci() {
+  let a = 1, b = 1;
+  while(true) {
+    yield a;
+    [a, b] = [b, a + b];
+  }
+}
+
+for(let x of qit(fibonacci()).take(10)) {
+  console.log(x);
+}
+```
+
+Basics
+----------------------------------------------------------------------
+
+qit function receives various iterable objects and returns wrapped qit iterators.
+
+### ArrayIterator
+
+```javascript
+// qit(array)
+var a = qit([1,2,3,4,5]);
+```
+
+### StringIterator
+
+```javascript
+// qit(string)
+var a = qit("ABCDE");
+// "A", "B", "C", "D", "E"
+```
+
+### ObjectIterator
+
+```javascript
+// qit(plain_object)
+var a = qit({a:1, b:2, c:3});
+// {key: "a", value: 1}, {key: "b", value: 2}, {key: "c", value: 3}
+```
+
+### RangeIterator
+
+```javascript
+// qit(first, limit)
+var a = qit(1, 5);
+// 1, 2, 3, 4, 5
+
+// qit(first, second, limit)
+var b = qit(2, 4, 10);
+// 2, 4, 6, 8, 10
+```
+
+### FollowIterator
+
+```javascript
+// qit(first, func)
+// qit(x, f) => [ x, f(x), f(f(x)), f(f(f(x))) ... ]
+var nats = qit(1, function(n) { return n+1 });
+// 1, 2, 3 ...
+```
+
+### Lazy methods
+
+#### map
+```javascript
+// .map(func)
+qit([1,2,3]).map(function(x){return x*2}).toArray();
+// [2,4,6]
+```
+
+#### filter
+```javascript
+// .filter(func)
+qit([1,2,3]).filter(function(x){return x%2 === 1}).toArray();
+// [1,3]
+```
+
+#### scan
+```javascript
+// .scan(func, initial_value = this.next().value)
+qit([1,2,3]).scan(function(a,b){return a+b}, 0).toArray();
+// [0+1,(0+1)+2,((0+1)+2)+3] == [1,3,6]
+
+qit([1,2,3]).scan(function(a,b){return a+b}).toArray();
+// [1+2,(1+2)+3] == [3,6]
+```
+
+#### take
+```javascript
+// .take(n)
+qit([1,2,3,4,5]).take(3).toArray();
+// [1,2,3]
+```
+
+#### drop
+```javascript
+// .drop(n)
+qit([1,2,3,4,5]).drop(3).toArray();
+// [4,5]
+```
+
+#### takeWhile
+```javascript
+// .takeWhile(func)
+qit([1,2,3,4,5]).takeWhile(function(x){return x<3}).toArray();
+// [1,2]
+```
+
+#### dropWhile
+```javascript
+// .dropWhile(func)
+qit([1,2,3,4,5]).dropWhile(function(x){return x<3}).toArray();
+// [3,4,5]
+```
+
+#### zip
+```javascript
+// .zip(iter1, iter2, iter3 ...)
+qit([1,2,3,4,5]).zip(['A','B','C']).toArray();
+// [[1,'A'],[2,'B'],[3,'C']]
+```
+
+#### zipWith
+```javascript
+// .zipWith(func, iter1, iter2, iter3 ...)
+qit([1,2,3,4,5]).zipWith(function(a,b){return b+a}, ['A','B','C']).toArray();
+// ['A1','B2','C3']
+```
+
+#### zipLongest
+```javascript
+// .zipLongest(iter1, iter2, iter3 ...)
+qit([1,2,3,4,5]).zipLongest(['A','B','C']).toArray();
+// [[1,'A'],[2,'B'],[3,'C'],[4,null],[5,null]]
+```
+
+#### zipWithLongest
+```javascript
+// .zipWithLongest(func, iter1, iter2, iter3 ...)
+qit([1,2,3,4,5]).zipWithLongest(function(a,b){return b+a}, ['A','B','C']).toArray();
+// ['A1','B2','C3',4,5]
+```
+
+#### concat
+```javascript
+// .concat(iter1, iter2, iter3 ...)
+qit([1,2,3,4,5]).concat(['A','B','C']).toArray();
+// [1,2,3,4,5,'A','B','C']
+```
+
+#### cycle
+```javascript
+// .cycle()
+qit([1,2,3]).cycle().take(8).toArray();
+// [1,2,3,1,2,3,1,2]
+```
+
+#### chunk
+```javascript
+// .chunk(n)
+qit(1,10).chunk(3).map(function(e){return e.toArray()}).toArray();
+// [[1,2,3],[4,5,6],[7,8,9],[10]]
+```
+
+#### buffered
+```javascript
+// .buffered(n)
+var it = qit(10,1).buffered(3);
+console.log(it.head(0)); // {value: 10, done:false}
+console.log(it.head(1)); // {value: 9, done:false}
+console.log(it.head(2)); // {value: 8, done:false}
+console.log(it.toArray());
+// [10,9,8,7,6,5,4,3,2,1]
+```
+
+### Eager methods
+
+#### each
+```javascript
+// .each(func)
+qit("Hello").each(function(c){
+  console.log(c);
+});
+```
+output:
+```javascript
+H
+e
+l
+l
+o
+```
+
+### reduce
+```javascript
+// .reduce(func, initial_value = this.next().value)
+qit([1,2,3,4,5]).reduce(function(a, b){
+  return a + b;
+}, 0);
+// ((((0+1)+2)+3)+4)+5 = 15
+```
+
+### toArray
+```javascript
+// .toArray()
+qit(1,10).toArray();
+// [1,2,3,4,5,6,7,8,9,10]
+```
+
+License
+----------------------------------------------------------------------
+
+Copyright (c) 2015 Hiroyuki OHARA Licensed under the MIT license.
---
+++ qit.js
@@ -0,0 +1,583 @@
+/*
+ * qit.js
+ *
+ * qit.js is a small iterator library for ES5/ES6
+ * providing lazy operations on iterator/generator
+ * compatible with ES6 iterator protocol.
+ *
+ * @version 0.9.0
+ * @author Hiroyuki OHARA <Hiroyuki.no22@gmail.com>
+ * @see https://github.com/no22/qit
+ */
+/*! qit.js v0.9.0 (c) 2015 Hiroyuki OHARA @license MIT */
+(function (root, globalName, factory) {
+  if (typeof define === "function" && define.amd) {
+    define([], factory);
+  } else if (typeof exports === "object") {
+    module.exports = factory();
+  } else {
+    root[globalName] = factory();
+  }
+})(this, "qit", function (undefined) {
+  "use strict"
+  // Helper functions
+  var getPrototypeOf = Object.getPrototypeOf, prototype = Object.prototype, slice = Array.prototype.slice,
+    toString = prototype.toString, id = Object().toString(), root = this || (0, eval)('this'),
+    symb = root.Symbol ? root.Symbol : {iterator: "@@iterator"}, qoo = root.qoo ? root.qoo : false ;
+
+  function isPlainObject(value) {
+    var proto, constructor;
+    if (!value || (typeof value !== 'object') || (toString.call(value) !== id)) {
+      return false;
+    }
+    proto = getPrototypeOf(value);
+    if (proto === null) {
+      constructor = value.constructor;
+      if (typeof constructor !== 'function') return true;
+      return (constructor.prototype !== value);
+    }
+    return (proto === prototype) || (getPrototypeOf(proto) === null);
+  };
+
+  function inherit(child, parent, obj) {
+    var proto = parent.prototype;
+    child.prototype = Object.create(proto, {
+      constructor: { value: child, enumerable: false, writable: true, configurable: true }
+    });
+    if (parent._inherit_) parent._inherit_.call(parent, child);
+    child.super_ = parent;
+    child._super_ = proto;
+    if (obj) methods(child, obj);
+    return child;
+  }
+
+  function methods(klass, obj) {
+    var k, proto = klass.prototype;
+    for (k in obj) proto[k] = obj[k];
+  }
+
+  function qit(iter, func, last) {
+    return new Iterator(iter, func, last);
+  }
+
+  function _iters(iters) {
+    var i, iterators = [];
+    for (var i = 0, len = iters.length; i < len; ++i) {
+      iterators.push(new Iterator(iters[i]));
+    }
+    return iterators;
+  }
+
+  // Iterator
+  function Iterator(iter, func, last) {
+    if (func !== undefined) {
+      if (typeof(func) === "function") {
+        this.iter = new FollowIterator(iter, func);
+      } else if (typeof iter === "number" && typeof func === "number") {
+        this.iter = new RangeIterator(iter, func, last);
+      }
+    } else if (iter instanceof Iterator) {
+      iter.rewind();
+      return iter;
+    } else if (typeof iter === "string") {
+      this.iter = new StringIterator(iter);
+    } else if (Array.isArray(iter)) {
+      this.iter = new ArrayIterator(iter);
+    } else if (isPlainObject(iter)) {
+      if (iter.next && typeof iter.next === "function") {
+        this.iter = iter;
+      } else {
+        this.iter = new ObjectIterator(iter);
+      }
+    } else if (iter[symb.iterator]) {
+      this.iter = iter[symb.iterator].call(iter);
+    } else {
+      this.iter = iter;
+    }
+    this.rewind();
+  }
+
+  Iterator.prototype[symb.iterator] = function() {
+    return this;
+  };
+
+  methods(Iterator, {
+    _next: function() {
+      if (this.finished) return {done: true};
+      var e = this.iter.next();
+      if (e.done) this.finished = true;
+      return e;
+    },
+    next: function() { return this._next(); },
+    rewind: function() {
+      this.finished = false;
+      if (this.iter && this.iter.rewind) this.iter.rewind();
+      return this;
+    },
+    each: function(func) {
+      var e;
+      while(e = this.next(), !e.done) func(e.value);
+    },
+    reduce: function(func, init) {
+      var e, acc = init;
+      if (init === undefined) {
+        e = this.next();
+        if (e.done) return acc;
+        acc = e.value;
+      }
+      while(e = this.next(), !e.done) {
+        acc = func(acc, e.value);
+      }
+      return acc;
+    },
+    toArray: function() {
+      return this.reduce(function(arr, e) {
+        arr.push(e);
+        return arr;
+      }, []);
+    },
+    head: function(n) { return undefined; },
+    concat: function(iters) { return new AppendIterator([this].concat(slice.call(arguments))); },
+    map: function(func) { return new MapIterator(this, func); },
+    take: function(n) { return new LimitIterator(this, 0, n); },
+    drop: function(n) { return new LimitIterator(this, n); },
+    filter: function(func) { return new FilterIterator(this, func); },
+    takeWhile: function(func) { return new TakeWhileIterator(this, func); },
+    dropWhile: function(func) { return new DropWhileIterator(this, func); },
+    scan: function(func, init) { return new ScanIterator(this, func, init); },
+    zipWith: function(func, iters) { return new ZipIterator([this].concat(slice.call(arguments,1)), func); },
+    zipWithLongest: function(func, iters) { return new ZipIterator([this].concat(slice.call(arguments,1)), func, true); },
+    zip: function(iters) { return new ZipIterator([this].concat(slice.call(arguments))); },
+    zipLongest: function(iters) { return new ZipIterator([this].concat(slice.call(arguments)),undefined,true); },
+    cycle: function() { return new CycleIterator(this); },
+    buffered: function(n) { return new BufferedIterator(this, n); },
+    chunk: function(n) { return new ChunkIterator(this, n); }
+  });
+
+  if (qoo) qoo.classify(Iterator);
+
+  // String Iterator
+  function StringIterator(str) {
+    this.str = str;
+    this.len = str.length;
+    this.rewind();
+  }
+
+  inherit(StringIterator, Iterator, {
+    rewind: function() {
+      this.idx = 0;
+      return StringIterator._super_.rewind.call(this);
+    },
+    next: function() {
+      if (this.idx >= this.len) {
+        this.finished = true;
+        return {done: true};
+      }
+      return {value: this.str[this.idx++], done: false};
+    }
+  });
+
+  // Array Iterator
+  function ArrayIterator(arr) {
+    this.arr = arr;
+    this.len = arr.length;
+    this.rewind();
+  }
+
+  inherit(ArrayIterator, Iterator, {
+    rewind: function() {
+      this.idx = 0;
+      return ArrayIterator._super_.rewind.call(this);
+    },
+    next: function() {
+      if (this.idx >= this.len) {
+        this.finished = true;
+        return {done: true};
+      }
+      return {value: this.arr[this.idx++], done: false};
+    }
+  });
+
+  // Object Iterator
+  function ObjectIterator(obj) {
+    this.obj = obj;
+    this.keys = Object.keys(obj);
+    this.len = this.keys.length;
+    this.rewind();
+  }
+
+  inherit(ObjectIterator, Iterator, {
+    rewind: function() {
+      this.idx = 0;
+      return ObjectIterator._super_.rewind.call(this);
+    },
+    next: function() {
+      if (this.idx >= this.len) {
+        this.finished = true;
+        return {done: true};
+      }
+      var key = this.keys[this.idx++];
+      return {value: {key: key, value: this.obj[key]}, done: false};
+    }
+  });
+
+  // Follow Iterator
+  function FollowIterator(first, func) {
+    this.first = first;
+    this.func = func;
+    this.rewind();
+  }
+
+  inherit(FollowIterator, Iterator, {
+    rewind: function() {
+      this.isFirst = true;
+      this.curr = null;
+      return FollowIterator._super_.rewind.call(this);
+    },
+    next: function() {
+      if (this.isFirst) {
+        this.isFirst = false;
+        this.curr = this.first === undefined ? this.func() : this.first ;
+      } else {
+        this.curr = this.func(this.curr);
+      }
+      if (this.curr === undefined) {
+        this.finished = true;
+        return {done: true};
+      }
+      return {value: this.curr, done: false};
+    }
+  });
+
+  // Range Iterator
+  function RangeIterator(first, second, last) {
+    this.first = first;
+    if (last === undefined) {
+      this.last = second;
+      this.step = first <= second ? 1 : -1 ;
+    } else {
+      this.last = last;
+      this.step = second - first;
+    }
+    this.rewind();
+  }
+
+  inherit(RangeIterator, Iterator, {
+    rewind: function() {
+      this.curr = this.first;
+      return RangeIterator._super_.rewind.call(this);
+    },
+    next: function() {
+      if ((this.step > 0 && this.curr > this.last) || (this.step < 0 && this.curr < this.last)) {
+        this.finished = true;
+        return {done: true};
+      }
+      var nextVal = this.curr;
+      this.curr += this.step;
+      return {value: nextVal, done: false};
+    }
+  });
+
+  // Append Iterator
+  function AppendIterator(iters) {
+    this.iters = _iters(iters);
+    this.rewind();
+  }
+
+  inherit(AppendIterator, Iterator, {
+    rewind: function() {
+      this.idx = 0;
+      this.len = this.iters.length;
+      for (var i = 0, len = this.len; i < len; ++i) {
+        this.iters[i].rewind();
+      }
+      return AppendIterator._super_.rewind.call(this);
+    },
+    next: function() {
+      var e = this.iters[this.idx].next();
+      if (!e.done) return e;
+      do {
+        if (++this.idx >= this.len) {
+          this.finished = true;
+          return {done: true};
+        }
+        e = this.iters[this.idx].next();
+      } while(e.done);
+      return e;
+    }
+  });
+
+  // Map Iterator
+  function MapIterator(iter, func) {
+    this.iter = iter;
+    this.func = func;
+    this.rewind();
+  }
+
+  inherit(MapIterator, Iterator, {
+    next: function() {
+      var e = this._next();
+      if (e.done) return e;
+      return {value: this.func(e.value), done: false};
+    }
+  });
+
+  // Limit Iterator
+  function LimitIterator(iter, offset, len, norewind) {
+    this.iter = iter;
+    this.offset = offset;
+    this.len = len;
+    this.norewind = norewind;
+    this.rewind();
+  }
+
+  inherit(LimitIterator, Iterator, {
+    rewind: function() {
+      this.isFirst = true;
+      this.idx = 0;
+      if (!this.norewind) return LimitIterator._super_.rewind.call(this);
+      this.finished = false;
+      return this;
+    },
+    next: function() {
+      var e, i;
+      if (this.isFirst) {
+        for (i = 0; i < this.offset; ++i) {
+          e = this._next();
+          if (e.done) return e;
+        }
+        this.isFirst = false;
+      }
+      if (this.len === undefined || this.idx < this.len) {
+        e = this._next();
+        this.idx++;
+        if (!e.done) return e;
+      }
+      this.finished = true;
+      return {done: true}
+    }
+  });
+
+  // Filter Iterator
+  function FilterIterator(iter, func) {
+    this.iter = iter;
+    this.func = func;
+    this.rewind();
+  }
+
+  inherit(FilterIterator, Iterator, {
+    next: function() {
+      var e;
+      do {
+        e = this._next();
+        if (e.done) return e;
+      } while(!this.func(e.value));
+      return e;
+    }
+  });
+
+  // TakeWhile Iterator
+  function TakeWhileIterator(iter, func) {
+    this.iter = iter;
+    this.func = func;
+    this.rewind();
+  }
+
+  inherit(TakeWhileIterator, Iterator, {
+    next: function() {
+      var e;
+      e = this._next();
+      if (e.done || this.func(e.value)) return e;
+      this.finished = true;
+      return {done: true};
+    }
+  });
+
+  // DropWhile Iterator
+  function DropWhileIterator(iter, func) {
+    this.iter = iter;
+    this.func = func;
+    this.rewind();
+  }
+
+  inherit(DropWhileIterator, Iterator, {
+    rewind: function() {
+      this.isFirst = true;
+      return DropWhileIterator._super_.rewind.call(this);
+    },
+    next: function() {
+      var e;
+      e = this._next();
+      if (e.done) return e;
+      if (this.isFirst) {
+        while(this.func(e.value)) {
+          e = this._next();
+          if (e.done) return e;
+        }
+        this.isFirst = false;
+      }
+      return e;
+    }
+  });
+
+  // Scan Iterator
+  function ScanIterator(iter, func, init) {
+    this.iter = iter;
+    this.func = func;
+    this.init = init;
+    this.rewind();
+  }
+
+  inherit(ScanIterator, Iterator, {
+    rewind: function() {
+      this.acc = null;
+      this.isFirst = true;
+      return ScanIterator._super_.rewind.call(this);
+    },
+    next: function() {
+      var e = this._next();
+      if (!e.done && this.isFirst) {
+        if (this.init === undefined) {
+          this.acc = e.value;
+          e = this._next();
+        } else {
+          this.acc = this.init;
+        }
+        this.isFirst = false;
+      }
+      if (e.done) return e;
+      this.acc = this.func(this.acc, e.value);
+      return {value: this.acc, done: false}
+    }
+  });
+
+  // Zip Iterator
+  function ZipIterator(iters, func, longest) {
+    this.iters = _iters(iters);
+    this.func = func;
+    this.longest = longest;
+    this.rewind();
+  }
+
+  inherit(ZipIterator, Iterator, {
+    rewind: function() {
+      var i, it, len = this.len = this.iters.length;
+      for (i = 0; i < len; ++i) {
+        this.iters[i].rewind();
+      }
+      return ZipIterator._super_.rewind.call(this);
+    },
+    next: function() {
+      var e, i, count = 0, len = this.len, args = [];
+      for (i = 0; i < len; ++i) {
+        e = this.iters[i].next();
+        if (e.done) {
+          count++;
+          if (!this.longest || count >= len) {
+            this.finished = true;
+            return e;
+          }
+          e.value = null;
+        }
+        args.push(e.value);
+      }
+      if (this.func === undefined) return {value: args, done: false};
+      return {value: this.func.apply(this, args), done: false};
+    }
+  });
+
+  // Cycle Iterator
+  function CycleIterator(iter) {
+    this.iter = iter;
+    this.rewind();
+  }
+
+  inherit(CycleIterator, Iterator, {
+    next: function() {
+      var e = this._next();
+      if (!e.done) return e;
+      this.rewind();
+      return this._next();
+    }
+  });
+
+  // Buffered Iterator
+  function BufferedIterator(iter, len) {
+    this.iter = iter;
+    this.len = len;
+    this.rewind();
+  }
+
+  inherit(BufferedIterator, Iterator, {
+    rewind: function() {
+      this.isFirst = true;
+      this.buffer = [];
+      return BufferedIterator._super_.rewind.call(this);
+    },
+    head: function(n) {
+      n = n === undefined ? 0 : n ;
+      if (this.isFirst) this.prepare();
+      return this.buffer[n];
+    },
+    prepare: function() {
+      var i,e;
+      for(i = 0; i < this.len; ++i) {
+        e = this.iter.next();
+        this.buffer.push(e);
+      }
+      this.isFirst = false;
+    },
+    next: function() {
+      if (this.finished) return {done: true};
+      if (this.isFirst) this.prepare();
+      var e = this.iter.next(), c = this.buffer.shift();
+      this.buffer.push(e);
+      if (c.done) this.finished = true;
+      return c;
+    }
+  });
+
+  // Chunk Iterator
+  function ChunkIterator(iter, len) {
+    this.iter = iter;
+    this.len = len;
+  }
+
+  inherit(ChunkIterator, Iterator, {
+    next: function() {
+      if (this.finished || this.iter.finished) {
+        this.finished = true;
+        return {done: true};
+      }
+      var e, it = (new LimitIterator(this.iter, 0, this.len, true)).buffered(1);
+      e = it.head();
+      if (e.done) {
+        this.finished = true;
+        return {done: true};
+      }
+      return {value: it, done: false};
+    }
+  });
+
+  qit.isPlainObject = isPlainObject;
+  qit.Iterator = Iterator;
+  qit.StringIterator = StringIterator;
+  qit.ArrayIterator = ArrayIterator;
+  qit.ObjectIterator = ObjectIterator;
+  qit.FollowIterator = FollowIterator;
+  qit.RangeIterator = RangeIterator;
+  qit.AppendIterator = AppendIterator;
+  qit.MapIterator = MapIterator;
+  qit.LimitIterator = LimitIterator;
+  qit.FilterIterator = FilterIterator;
+  qit.TakeWhileIterator =  TakeWhileIterator;
+  qit.DropWhileIterator = DropWhileIterator;
+  qit.ScanIterator = ScanIterator;
+  qit.ZipIterator = ZipIterator;
+  qit.CycleIterator = CycleIterator;
+  qit.BufferedIterator = BufferedIterator;
+  qit.ChunkIterator = ChunkIterator;
+  qit.Symbol = symb;
+
+  return qit;
+});
